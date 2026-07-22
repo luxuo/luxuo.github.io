39745d1d7ee40787b419d74b8e2d6dca927cc871
Émile <emile.hogue.03@gmail.com>
2026-07-22 18:48:06 -0400

    refactor mineur

--- src/compression_algos/compressor.py
+++ src/compression_algos/compressor.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
 class Compressor(ABC):
     @abstractmethod
-    def C(self, str) -> str: 
+    def C(self, str:str) -> str: 
         pass
 
     @abstractmethod
-    def D(self, str) -> str:
+    def D(self, str:str) -> str:
         pass
