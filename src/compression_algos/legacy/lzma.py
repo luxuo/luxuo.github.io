68ee983cbe97f2f5a9d5c57ec43d93e3c9b75a92
Émile <emile.hogue.03@gmail.com>
2026-07-22 18:47:16 -0400

    skelette polymorphique

---
+++ src/compression_algos/legacy/lzma.py
@@ -0,0 +1,8 @@
+from compressor import Compressor
+
+class LZMA(Compressor):
+    def C(self, str:str) -> str:
+        return ''
+
+    def D(self, str:str) -> str:
+        return ''
---
+++ src/compression_algos/legacy/lzw.py
@@ -0,0 +1,8 @@
+from compressor import Compressor
+
+class LZW(Compressor):
+    def C(self, str:str) -> str:
+        return ''
+
+    def D(self, str:str) -> str:
+        return ''
