91d0bddb7711e8924ebaa4e704cf0c7e7cdb079a
Émile <emile.hogue.03@gmail.com>
2026-07-30 19:42:11 -0400

    implémentation des représentations

--- src/tests/README.md
+++ src/tests/README.md
@@ -1,6 +1,4 @@
 # TODO
-- implémenter des tests à vérifier le fonctionnement des représentations numériques
-- implémenter des tests à vérifier le jumelage des représentations numériques sur les distances de compression
-- implémenter des tests pour vérifier le bon fonctionnement des algorithmes de compression
+- implémenter plusieurs tests pertinents
 # Comment effectuer les tests
 `$/src python3 -m unittest discover -s ./tests/`
--- src/tests/test_representations.py
+++ src/tests/test_representations.py
@@ -1,7 +1,11 @@
 from representations.numerical import nncr
-from compression_distances.clm import CLM
+from compression_distances.cdm import CDM
 from compression_algos.custom_impasse import Impasse
+import matplotlib.pyplot as plt
 import unittest
+
+METRIC = CDM()
+COMPRESSOR = Impasse
 
 class TestNaturalNumberCompressionRepresentation(unittest.TestCase):
     def test_functionnality(self):
@@ -10,19 +14,42 @@ class TestNaturalNumberCompressionRepresentation(unittest.TestCase):
         self.assertEqual(len(nncr(0)), 0)
 
     def test_different_compression_length(self):
-        compressor = Impasse
-        metric = CLM()
-        self.assertLess(metric.dist(compressor, nncr(3), nncr(14), break_string=True), metric.dist(compressor, nncr(3), nncr(50), break_string=True))
-        self.assertLess(metric.dist(compressor, nncr(3), nncr(3), break_string=True), metric.dist(compressor, nncr(3), nncr(-3), break_string=True))
-        #self.assertLess(metric.dist(compressor, nncr(3), nncr(4), break_string=True), metric.dist(compressor, nncr(3), nncr(5), break_string=True))
+        self.assertLess(METRIC.dist(COMPRESSOR, nncr(3), nncr(4), break_string=True), METRIC.dist(COMPRESSOR, nncr(3), nncr(50), break_string=True))
+        #self.assertLess(METRIC.dist(COMPRESSOR, nncr(5), nncr(5), break_string=True), METRIC.dist(COMPRESSOR, nncr(4), nncr(-14), break_string=True))
+        #self.assertLess(METRIC.dist(COMPRESSOR, nncr(3), nncr(4), break_string=True), METRIC.dist(COMPRESSOR, nncr(3), nncr(5), break_string=True))
+        
+
+    def test_compression_deltas(self):
         deltas = []
-        for i in range(14):
-            deltas.append(metric.dist(compressor,nncr(i),nncr(i+1), True))
-        print(deltas)
-        deltas = []
-        for i in range(14):
-            deltas.append(metric.dist(compressor,nncr(3),nncr(i), True))
-        print(deltas)
+        x = range(100)
+        for i in x:
+            deltas.append(METRIC.dist(COMPRESSOR,nncr(i),nncr(i+1), True))
+        fig, ax = plt.subplots()
+
+        ax.plot(x, deltas)
+
+        ax.set(xlabel='Chiffre', ylabel='Delta distance au prochain chiffre',
+        title='Calcul des "dérivées" de distance de compression des représentations numériques')
+        ax.grid()
+
+        #fig.savefig("./tests/plots/compression_deltas.png")
+
+    def test_compression_distances(self):
+            number = 50
+            deltas = []
+            x = range(100)
+            for i in x:
+                deltas.append(METRIC.dist(COMPRESSOR,nncr(number),nncr(i), True))
+            fig, ax = plt.subplots()
+    
+            ax.plot(x, deltas)
+    
+            ax.set(xlabel='Chiffre', ylabel='Distance de ' + str(number),
+            title='Distance entre valeurs numériques et ' + str(number))
+            ax.grid()
+    
+            fig.savefig("./tests/plots/compression_distance_to" + str(number) + ".png")
+
 
 def run_tests():
     unittest.main()
