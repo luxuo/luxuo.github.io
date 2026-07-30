30931f0da586633910b5991c47df17a6e0a57c85
Émile <emile.hogue.03@gmail.com>
2026-07-30 19:43:37 -0400

    travail sur dgc

--- src/compression_algos/custom_dgc.py
+++ src/compression_algos/custom_dgc.py
@@ -1,16 +1,100 @@
+from collections import Counter
+import math
+
+def merge_characters(ancestor, prev_dict, prev_dict_index):
+    # make disappear the character
+    # search for all occurrences of ancestor
+    # ancestors -len(ancestor)
+
+    new_prev_dict = prev_dict.copy()
+    new_prev_index_dict = prev_dict_index.copy()
+
+    for successor in prev_dict.keys():
+        
+        ancestor_index = prev_dict[successor].index(ancestor) if ancestor in prev_dict[successor] else -1
+        while ancestor_index != -1:
+            # remove ancestor from successor
+            # remove ancestor index from successor
+            successor_ancestor_index = prev_dict_index[successor].pop(ancestor_index)
+            new_ancestor_index = successor_ancestor_index - len(prev_dict[successor].pop(ancestor_index))
+            new_ancestor_index_index = prev_dict_index[ancestor].index(new_ancestor_index)
+            prev_dict_index[ancestor].pop(new_ancestor_index_index)
+            # remove new ancestor from ancestor
+            new_ancestor = prev_dict[ancestor].pop(new_ancestor_index_index)
+
+            # cleanup dict if possible TODO NEEDS ANOTHER CHECK FOR CHAR
+            if len(prev_dict[successor]) == 0:
+                del new_prev_dict[successor]
+            if len(prev_dict_index[successor]) == 0:
+                del new_prev_index_dict[successor]
+            if len(prev_dict[ancestor]) == 0:
+                del new_prev_dict[ancestor]
+            if len(prev_dict_index[ancestor]) == 0:
+                del new_prev_index_dict[ancestor]
+
+            # insert combined ancestor-successor with index
+            new_key = ancestor + successor
+            # new key insertion
+            if new_key not in new_prev_dict:
+                new_prev_dict[new_key] = []
+                new_prev_index_dict[new_key] = []
+            new_prev_dict[new_key].append(new_ancestor)
+            new_prev_index_dict[new_key].append(new_ancestor_index)
+
+            # replace successor's successor's ancestor as ancestor-successor
+            successor_successor_index = successor_ancestor_index + len(successor)
+             # find successor
+            new_prev_index_dict[][new_prev_index_dict.index()]
+
+
+            # update ancestor_index
+            ancestor_index = prev_dict[successor].index(ancestor) if ancestor in prev_dict[successor] else -1
+    return new_prev_dict, new_prev_index_dict
+
+
+
+
 def createAncestorDict(s):
+    counter = Counter(s)
     prev_dict = {}
-    for i in range(len(s)-1):
+    prev_index_dict = {}
+
+    for i in range(len(s)-1): # O(n)
         # new key in dict
         if s[i+1] not in prev_dict.keys():
             prev_dict[s[i+1]] = []
+            prev_index_dict[s[i+1]] = []
         
         # add prev
         prev_dict[s[i+1]].append(s[i])
+        prev_index_dict[s[i+1]].append(i)
 
-        # check
-        # TODO
+    # calculate entropy of next characters
+    entropy_dict = {}
+    for ancestor in counter.keys(): # O(n²)
+        if ancestor not in entropy_dict:
+            entropy_dict[ancestor] = 0
+        for count_from in counter.keys():
+            char_prob = Counter(prev_dict[count_from])[ancestor] / counter[ancestor]
+            entropy_dict[ancestor] -= 0 if char_prob == 0 else char_prob * math.log2(char_prob)
 
-    return prev_dict
+    # FIRST VERSION: check for zeroes
+    for e in entropy_dict.keys():
+        if entropy_dict[e] == 0.0:
+            # merge characters
+            print(prev_dict)
+            print(prev_index_dict)
+            print()
+            prev_dict, prev_index_dict = merge_characters(e, prev_dict, prev_index_dict)
+            break
+            
+    # merge characters
+    # repeat till random
+    # TODO
 
-print(createAncestorDict('tobeornottobeortobeornot'))
+    return prev_dict, prev_index_dict, counter
+
+p, i, c = createAncestorDict('tobeornottobeortobeornot')
+print(p)
+print(i)
+print(c)
--- src/compression_distances/cdm.py
+++ src/compression_distances/cdm.py
@@ -3,8 +3,8 @@ from compression_distances.metric import Metric
 
 # Compression-based dissimilarity measurement citation indirecte https://arxiv.org/pdf/2206.11573
 class CDM(Metric):
-    def dist(self, C: Compressor, x: str, y: str) -> float:
+    def dist(self, C: Compressor, x: str, y: str, break_string:bool=False, break_char:str='*') -> float:
         cx = len(C.C(x))
         cy = len(C.C(y))
-        cxy = len(C.C(x+y))
+        cxy = len(C.C(x+y)) if not break_string else len(C.C(x+break_char+y))
         return float(cx + cy) / cxy
