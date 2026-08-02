fa5b5d99307fb5bece4bd6915ea53740325b3926
Émile <emile.hogue.03@gmail.com>
2026-08-02 13:11:22 -0400

    enlèvement de fichiers non-nécessaires

--- src/__main__.py
+++ src/__main__.py
@@ -3,14 +3,14 @@ from benchmark.convert_datasets import load_compress_wine, load_compress_iris, l
 from compression_algos.legacy_lzw import LZW
 
 
-#test_representations.run_tests()
+test_representations.run_tests()
 
 #load_compress_wine()
 #load_compress_iris()
 #load_compress_breast_cancer()
 
-s = 'tobeornottobeortobeornot'
-c = LZW()
-a = c.C(s)
-print(a)
-print(c.C_len(s), len(s) * 8)
+# s = 'tobeornottobeortobeornot'
+# c = LZW()
+# a = c.C(s)
+# print(a)
+# print(c.C_len(s), len(s) * 8)
